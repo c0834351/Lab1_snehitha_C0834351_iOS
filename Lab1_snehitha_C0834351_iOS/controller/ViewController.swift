39bd466873d7ca7ffab0c325bc3c87251569d758
c0834351 <c0834351@mylambton.ca>
2022-01-24 23:15:45 -0500

    tried

--- Lab1_snehitha_C0834351_iOS/controller/ViewController.swift
+++ Lab1_snehitha_C0834351_iOS/controller/ViewController.swift
@@ -6,6 +6,13 @@
 //
 
 import UIKit
+import CoreData
+
+
+var tictac: [NSManagedObject] = []
+var player2: [NSManagedObject] = []
+var undo = UndoManager()
+let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
 class ViewController: UIViewController {
 
@@ -15,19 +22,26 @@ class ViewController: UIViewController {
     var positions = WinningPositions()
     var n1=0
     var c1=0
-    
+    var saveN: [Int] = []
+    var btn: UIButton!
+    var d1:String = "0"
+    var d2:String = "0"
     
     
     @IBOutlet weak var winner: UILabel!
     @IBOutlet weak var crossesLb: UILabel!
     @IBOutlet weak var noughtsLb: UILabel!
     
+    
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        becomeFirstResponder()
         
+        noughtsLb.text = d1
+        crossesLb.text = d2
         let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(swiped))
         swipeLeft.direction = .left
         view.addGestureRecognizer(swipeLeft)
@@ -50,6 +64,7 @@ class ViewController: UIViewController {
 //        crossesLb.text = "0"
         
     }
+    
 
     //play again or reset the game if u swipe left/right/up/down
     @objc func swiped(gesture: UISwipeGestureRecognizer){
@@ -75,20 +90,43 @@ class ViewController: UIViewController {
         }
         
     }
+    ///////////
+    
     
     @IBAction func buttonsPressed(_ sender: UIButton) {
+        btn = sender
         let activePosition = sender.tag-1
+        print("\(sender.tag)@@@@@@@@@@@@@@@@@")
+//        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else{
+//            return
+//        }
+//        let context = appDelegate.persistentContainer.viewContext
+//        let entity = NSEntityDescription.entity(forEntityName:"Tictactoe", in:context)!
+//        let record = NSManagedObject(entity:entity, insertInto:context)
+//        record.setValue(String(sender.tag), forKey:"gameState")
+//        saveData()
+        
         if states.gameStates[activePosition] == 0 && gameActive{
             states.gameStates[activePosition] = player
         addToGame(sender)
+            
         }
     }
     // logic for displaying the cross and noughts
     func addToGame(_ sender: UIButton){
         if(sender.image(for: .normal)==nil){
             if(player == 1){
+                guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else{
+                    return
+                }
+                let context = appDelegate.persistentContainer.viewContext
+                let entity = NSEntityDescription.entity(forEntityName:"Tictactoe", in:context)!
+                let record = NSManagedObject(entity:entity, insertInto:context)
+                record.setValue(String(sender.tag), forKey:"gameState")
+                saveData()
                 sender.setImage(UIImage(named: "nought.png"), for: [])
                 player = 2
+                
                 sender.alpha=0.5
                 sender.transform = CGAffineTransform(translationX: -200, y: 0)
                 UIView.animate(withDuration: 0.5) {
@@ -96,6 +134,15 @@ class ViewController: UIViewController {
                 }
             }
             else{
+                guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else{
+                    return
+                }
+                let context = appDelegate.persistentContainer.viewContext
+                let entity = NSEntityDescription.entity(forEntityName:"Player2", in:context)!
+                let record = NSManagedObject(entity:entity, insertInto:context)
+                record.setValue(String(sender.tag), forKey:"gameState")
+                saveData()
+                
                 sender.setImage(UIImage(named: "cross.png"), for: [])
                 player = 1
                 sender.transform = CGAffineTransform(translationX: 0, y: -150)
@@ -114,14 +161,50 @@ class ViewController: UIViewController {
                 if states.gameStates[position[0]] == 1{
                     winner.isHidden=false
                     winner.text = "Player 1 (Noughts) is winner"
-                    n1+=1
-                    noughtsLb.text = String(n1)
+                    print("\(tictac.count) -------" )
+                    ///
+                    guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else{
+                        return
+                    }
+                    let context = appDelegate.persistentContainer.viewContext
+                    let entity = NSEntityDescription.entity(forEntityName:"Tictactoe", in:context)!
+                    let record = NSManagedObject(entity:entity, insertInto:context)
+                    n1 += 1
+                    record.setValue(String(n1), forKey:"score")
+                    
+                    //let c = tictac[tictac.count-1]
+//                    let s = (String(describing: c.value(forKey: "score"))
+//                    n1 = n1 + Int(s)
+                  //  n1 = Int(String(describing: c.value(forKey: "score") as! NSString).integerValue
+                    saveData()
+                    fetchData1()
+                    print("\(tictac.count) -------" )
+                    let sc = tictac[tictac.count-1]
+                    noughtsLb.text = String(describing: sc.value(forKey: "score") ?? "0")
+                    d1 = String(describing: sc.value(forKey: "score") ?? "0")
+                    print(String(describing: sc.value(forKey: "score")))
+                    
+                    //noughtsLb.text = String(n1)
                 }
                 else{
                     winner.isHidden=false
                     winner.text = "Player 2 (Crosses) is winner"
-                    c1+=1
-                    crossesLb.text = String(c1)
+                    c1 += 1
+                    guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else{
+                        return
+                    }
+                    let context = appDelegate.persistentContainer.viewContext
+                    let entity = NSEntityDescription.entity(forEntityName:"Player2", in:context)!
+                    let record = NSManagedObject(entity:entity, insertInto:context)
+                    record.setValue(String(c1), forKey:"score")
+                    saveData()
+                    fetchData2()
+                    let sc = player2[player2.count-1]
+                    crossesLb.text = String(describing: sc.value(forKey: "score") ?? "0")
+                    d2 = String(describing: sc.value(forKey: "score") ?? "0")
+                    
+                    ///
+                   // crossesLb.text = String(c1)
                 }
             }
         }
@@ -131,7 +214,67 @@ class ViewController: UIViewController {
         }
     }
     
+    //fetching data
+    func fetchData1(){
+      //  let fetchRequest = NSFetchRequest < NSManagedObject > (entityName: "Tictactoe")
+        let fetchRequest = NSFetchRequest<NSManagedObject>.init(entityName: "Tictactoe")
+        do {
+           tictac =
+            try context.fetch(fetchRequest)
+        } catch
+        let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+}
     
+    func fetchData2(){
+      //  let fetchRequest = NSFetchRequest < NSManagedObject > (entityName: "Tictactoe")
+        let fetchRequest = NSFetchRequest<NSManagedObject>.init(entityName: "Player2")
+        do {
+           player2 =
+            try context.fetch(fetchRequest)
+        } catch
+        let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+}
+    
+    
+    //
+    func saveData() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving the folder \(error.localizedDescription)")
+        }
+    }
+    
+    //shake motion
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if event?.subtype == .motionShake{
+            print("phone shake")
+            positions.gameStates = saveN
+            print("\(positions.gameStates) \(btn.tag) ")
+            if player == 1{
+                player = 2
+            }
+            else
+            {
+                player=1
+            }
+            btn.setImage(nil, for: .normal)
+            
+        }
+        
+        //delete
+        do{
+       // self.context.delete(self.details[indexPath.row])
+            try context.save()
+        }catch{
+            print(error)
+        }
+        
+    }
     
     
     // if its a tie match (no one wins)
